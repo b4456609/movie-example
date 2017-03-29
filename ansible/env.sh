4759bace9c00e6c446e8c2bd99039294b8c5643d
Bernie <b4456609@gmail.com>
2017-03-29 09:25:30 +0800

    add ansible

---
+++ ansible/env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export MGP=localhost:4000
+export PACT_URL=http://localhost:8880/      
+export DB_HOST=localhost
+export TARGET=movie
+export MOVIE_HOST=localhost         
+export MOVIE_PORT=8081
+export ORDER_HOST=localhost
+export ORDER_PORT=8083
+export THEATER_HOST=localhost
+export THEATER_PORT=8084
+export USER_HOST=localhost
+export USER_PORT=8082
